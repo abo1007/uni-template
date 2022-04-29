7448f2ede232e9a89d41fb6512733c248be1928a
bo yang <1003208172@qq.com>
2022-04-29 23:47:18 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # uni-template
 uni-template uniapp基本模板
+
+## 现在有什么
+
+* http请求封装
+* 全局统一URL
+* Vuex引入
+
+
--- main.js
+++ main.js
@@ -2,12 +2,14 @@ import App from './App'
 
 import apiList from "./util/apiList.js"
 import store from "./util/store.js"
+import req from "./util/request.js"
 // #ifndef VUE3
 import Vue from 'vue'
 Vue.config.productionTip = false
 App.mpType = 'app'
 Vue.prototype.$apiList = apiList;
 Vue.prototype.$store = store;
+Vue.prototype.$baseUrl = req.baseUrl;
 const app = new Vue({
     ...App
 })
